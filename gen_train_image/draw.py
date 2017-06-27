e985f7b316ff2b8063c3fdfaa6fe4a4607c9ccf7
yunxz <ih.c.frv@gmail.com>
2017-06-27 09:37:03 +0800

    add draw_quadrants()

--- gen_train_image/draw.py
+++ gen_train_image/draw.py
@@ -12,11 +12,112 @@ import copy
 
 
 def check(x, y):
-    if (x < start_point) or \
-            (y < start_point):
-        return 0
+    if (x < start_point) or (x > canvas_width + start_point)\
+            or (y < start_point) or (y > canvas_height + start_point):
+        return FALSE
     else:
-        return 1
+        return TRUE
+
+
+def draw_quadrant(n_quadrants, n_steps, n_images):
+    image_counter = 0
+    while image_counter < n_images:
+        moves = []
+        rows = [[FALSE for x in range(row_num)] for y in range(row_num + 1)]
+        cols = [[FALSE for x in range(row_num)] for y in range(row_num + 1)]
+        quadrants = [FALSE for x in range(5)]
+        p0_x, p0_y = line_dis * random.randint(0, row_num), line_dis * random.randint(0, row_num)
+        start_quadrant = get_quadrant((p0_x, p0_y))
+        quadrants_count = 1
+        quadrants[start_quadrant] = TRUE
+        steps_counter = 0
+        while steps_counter < n_steps:
+            flag_quadrant_check = FALSE
+            flag_line_redundancy_check = FALSE
+            for i in range(20):
+                d = random.choice([-1, 1])
+                coin = bool(random.getrandbits(1))
+                if coin:
+                    p1_x = p0_x + line_dis * d
+                    p1_y = p0_y
+                else:
+                    p1_x = p0_x
+                    p1_y = p0_y + line_dis * d
+
+                flag_quadrant_check = check_quadrants((p1_x, p1_y), quadrants_count, n_quadrants, quadrants)
+                flag_line_redundancy_check = check_line_redundancy((p1_x, p1_y), rows, cols, coin, d)
+                if flag_line_redundancy_check and flag_quadrant_check:
+                    break
+
+            if flag_quadrant_check and flag_line_redundancy_check:
+                p_q = get_quadrant((p1_x, p1_y))
+                if quadrants[p_q] is FALSE:
+                    quadrants_count += 1
+                    quadrants[p_q] = TRUE
+                if coin:
+                    rows[p1_y//line_dis][(p1_x-max(d, 0))//line_dis] = TRUE
+                else:
+                    cols[p1_x//line_dis][(p1_y-max(d, 0))//line_dis] = TRUE
+                moves.append([p0_x, p0_y, p1_x, p1_y])
+                p0_x = p1_x
+                p0_y = p1_y
+                steps_counter += 1
+            else:
+                break
+
+        if steps_counter == n_steps and quadrants.count(True) == n_quadrants:
+            image_counter += 1
+            im = Image.new("RGB", (64, 64), (255, 255, 255))
+            draw = ImageDraw.Draw(im)
+            for x in range(0, 65, 8):
+                draw.line([x, 0, x, 64], rgb_blk)
+            for y in range(0, 65, 8):
+                draw.line([0, y, 64, y], rgb_blk)
+            for i in range(n_steps):
+                draw.line(moves[i], fill=rgb_red, width=bold_width)
+            im.save("images/" + str(image_counter) + ".png")
+
+
+def check_quadrants(p, q_count, n_q, qs):
+    if check(p[0], p[1]) is FALSE:
+        return FALSE
+
+    if n_q < q_count:
+        return FALSE
+
+    p_q = get_quadrant(p)
+
+    if qs[p_q]:
+        return TRUE
+    elif q_count < n_q:
+        return TRUE
+    else:
+        return FALSE
+
+
+def check_line_redundancy(p1, rows, cols, coin, d):
+    if check(p1[0], p1[1]) is FALSE:
+        return FALSE
+    if coin:
+        return rows[p1[1]//line_dis][p1[0]//line_dis - max(d, 0)] is not TRUE
+    else:
+        return cols[p1[0]//line_dis][p1[1]//line_dis - max(d, 0)] is not TRUE
+
+
+
+def get_quadrant(p):
+    mid_y = canvas_height // 2
+    mid_x = canvas_width // 2
+    if p[0] <= mid_x:
+        if p[1] <= mid_y:
+            return 2
+        else:
+            return 3
+    else:
+        if p[1] > mid_y:
+            return 1
+        else:
+            return 4
 
 
 def checkered(canvas, line_distance, loop_num, draw):
@@ -57,8 +158,8 @@ def generate():
     init_x = canvas_height / 2
     init_y = canvas_width / 2
     index = [[0 for x in range(math.ceil((row_num + 1) / 2))] for y in range(math.ceil((row_num + 1) / 2))]
-    draw_to_centre(image1, init_x, init_y, index)
-    distort()
+    #draw_to_centre(image1, init_x, init_y, index)
+    # distort()
 
 
 # all the paths end at the centre, though they are in fact extended from the centre
@@ -108,17 +209,17 @@ def distort():
         for i in range(0, 10):
             im = Image.open("images/" + fn)
             if bool(random.getrandbits(1)):
-                x, y = random.randint(canvas_width / 4, canvas_width * 3/4), \
-                       random.randint(canvas_height / 4, canvas_height * 3/4)
+                x, y = random.randint(canvas_width / 4, canvas_width * 3 / 4), \
+                       random.randint(canvas_height / 4, canvas_height * 3 / 4)
                 im = im.rotate(random.randint(-15, 15), resample=PIL.Image.BILINEAR, center=[x, y])
             else:
                 data = np.array(im)
                 if bool(random.getrandbits(1)):
                     r0, g0, b0 = rgb_red
-                    r1, g1, b1 = random.randint(0, 128), random.randint(0, 255), random.randint(0, 255)
+                    r1, g1, b1 = random.randint(225, 255), random.randint(0, 30), random.randint(0, 30)
                 else:
                     r0, g0, b0 = rgb_blk
-                    r1, g1, b1 = random.randint(10, 255), random.randint(10, 255), random.randint(10, 255)
+                    r1, g1, b1 = random.randint(0, 20), random.randint(0, 20), random.randint(0, 20)
 
                 red, green, blue = data[:, :, 0], data[:, :, 1], data[:, :, 2]
                 mask = (red == r0) & (green == g0) & (blue == b0)
@@ -127,6 +228,8 @@ def distort():
 
             im.save("images/d" + str(distort.counter) + ".png")
             distort.counter += 1
+
+
 distort.counter = 0
 
 if __name__ == "__main__":
@@ -158,4 +261,5 @@ if __name__ == "__main__":
         # w.create_line(start_point, y, canvas_width, y, fill="#476042")
         draw.line([start_point - start_point, y - start_point, canvas_width - start_point, y - start_point], rgb_blk)
 
-    generate()
+    #generate()
+    draw_quadrant(2, 12, 50000)
